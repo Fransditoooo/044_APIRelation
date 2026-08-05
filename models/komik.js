d77cc7aa63b7b1a673e566d86dc30c3639c5ddc7
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-08-05 14:32:28 +0700

    menambahkan atribut judul pada model Komik

--- models/komik.js
+++ models/komik.js
@@ -5,4 +5,8 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true
         },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         
